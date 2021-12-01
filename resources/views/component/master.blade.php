97732f3b62008c0c5517caa5fd9498dd64952630
FurqonWidaya <55239325+FurqonWidaya@users.noreply.github.com>
2021-12-01 11:13:03 +0700

    ..

---
+++ resources/views/component/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Inter:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('css/main.css')}}" rel="stylesheet" type="text/css">
+
+<head>
+  <title></title>
+</head>
+<body>
+@yield('content')
+</body>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</html>
