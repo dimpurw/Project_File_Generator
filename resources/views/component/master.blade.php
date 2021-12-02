10041631663bc1d17f888f45288683380dafe2fa
JupriTupi <luthfidians@gmail.com>
2021-12-02 16:47:02 +0700

    Update generate.blade.php

--- resources/views/component/master.blade.php
+++ resources/views/component/master.blade.php
@@ -1,48 +1,18 @@
 <!DOCTYPE html>
 <html>
-<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Inter:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-<!-- <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css"> -->
-<!-- <link href="{{asset('css/bootstrap2.css')}}" rel="stylesheet" type="text/css"> -->
+<link href="{{asset('css/bootstrap2.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('css/main.css')}}" rel="stylesheet" type="text/css">
 
 <head>
-  <title>FILE GENERATOR</title>
+  <title></title>
 </head>
 <body>
-<div id="wrapper">
-	<div id="content-wrapper" class="d-flex flex-column">
-		@yield('content')
-	</div>
-</div>
+@yield('content')
 </body>
-<script>
-  function listIcon(divTag, id) {
-    if (document.getElementById(divTag+id).style.display == 'none') {
-          document.getElementById(divTag+id).style.display = 'block';
-        } else {
-          document.getElementById(divTag+id).style.display = 'none';
-    };
-  }
-  function deleteList(id) {
-    var del = document.getElementById("list-surat"+id);
-    var iconD = document.getElementById("icon-display"+id);
-    var deskrip = document.getElementById("deskrip-surat"+id);
-    var viewDelList = document.getElementById("del-surat"+id);
-    del.style.backgroundColor ='#FDC0C0';
-    del.style.borderLeftColor ='#FF3C3C';
-    iconD.style.display = 'none';
-    deskrip.style.display = 'none';
-    viewDelList.style.display = 'block';
-  }
-  
-</script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
- <script src="http://code.jquery.com/jquery-3.3.1.min.js"
-               integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-               crossorigin="anonymous">
-</script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </html>
--- resources/views/user/createtemplate.blade.php
+++ resources/views/user/createtemplate.blade.php
@@ -1,207 +1,110 @@
-<link href="{{asset('css/bootstrap2.css')}}" rel="stylesheet" type="text/css">
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<!-- <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css"> -->
 @extends('component.master')
 @section('content')
 <style>
-.content{
-	color: #7A7A7A;
-}
 .bg-warning{
 	/*width: 1440px;*/
 	height: 96px;
 }
 .container-fluid{
 	width: 80%;
-	margin-top: 80px;	
 }
 .card{
 	height: 100%;
 }
-.box{
-	 width: 50px;
-	 height: 50px;
-	 padding-top: 10px;
-	 margin-right: 10px;
-}
-.active{
-	background-color: #FFE5B6;
-}
 </style>
-<div class="content">
 <!-- Navbar-->
 <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-	<a class="navbar-brand" href="#" ><img src="{{asset('../asset/img/back.png')}}" style="width: 64px"></a>
-	<div class="container">
-		
-		<div class="collapse navbar-collapse" id="navcol-1">
-			<div class="nav navbar-nav w-100 justify-content-between">
-				<div class="nav-item"><p>Text Option</p>
-					<div class="row col-lg-12">
-						<select class="custom-select col-5">
+	<div class="d-flex justify-content-around">
+
+		<div class="d-flex justify-content-start">
+			<div class="p-2 bd-highlight">Back
+			</div>
+		</div>
+
+		<div class="d-flex justify-content-center">
+			<div class="p-2 bd-highlight">
+				<select id="select_font" onchange="changeFont(this);">
 					<option value="10">10</option>
-					<option value="11">11</option>
+					<option value="11" selected>11</option>
 					<option value="12">12</option>
 					<option value="13">13</option>
 					<option value="14">14</option>
 					<option value="15">15</option>
-					<option value="16" selected>16</option>
+					<option value="16">16</option>
 					<option value="17">17</option>
 					<option value="18">18</option>
 					<option value="19">19</option>
 					<option value="20">20</option>
 				</select>
-				<select class="custom-select col-7">
+			</div>
+			<div class="p-2 bd-highlight">
+				<select id="select_font" onchange="changeFont(this);">
 					<option value="Bold">Bold</option>
 					<option value="Regular" selected>Regular</option>
 					<option value="Italic">12</option>
 				</select>
-					</div>
-				</div>
-				<div class="nav-item"><p>Line Height and Spacing</p>
-					<select class="custom-select col-5">
-						<option value="16">16</option>
-					</select>
-					<select class="custom-select col-5">
-						<option value="4%">4%</option>
-					</select>
-				</div>
-				<div class="nav-item"><p>Paragraph spacing and Alignment</p>
-					<select class="custom-select col-5">
-						<option value="auto">Auto</option>
-					</select>
-					<select class="custom-select col-5">
-						<option value="left">Left</option>
-					</select>
-				</div>
-				<div class="nav-item" >
-					<div class="d-flex align-items-center" style="padding-top: 23px">
-					<a class="box btn" href="#" ><img src="{{asset('../asset/img/view.png')}}" style="width: 28px;"></a>
-					<a class="box btn active" href="#" ><img src="{{asset('../asset/img/edit.png')}}" style="width: 28px;"></a>
-					<a class="box btn" href="#" ><img src="{{asset('../asset/img/delete.png')}}" style="width: 28px"></a>
-					</div>
-				</div>
 			</div>
 		</div>
+
+		<div class="d-flex justify-content-center">
+			<div class="p-2 bd-highlight">
+				<select id="select_font" onchange="changeFont(this);">
+					<option value="10">10</option>
+					<option value="11" selected>11</option>
+					<option value="12">12</option>
+					<option value="13">13</option>
+					<option value="14">14</option>
+					<option value="15">15</option>
+					<option value="16">16</option>
+					<option value="17">17</option>
+					<option value="18">18</option>
+					<option value="19">19</option>
+					<option value="20">20</option>
+				</select>
+			</div>
+			<div class="p-2 bd-highlight">
+				<select id="select_font" onchange="changeFont(this);">
+					<option value="Bold">Bold</option>
+					<option value="Regular" selected>Regular</option>
+					<option value="Italic">12</option>
+				</select>
+			</div>
+		</div>
+
 	</div>
 </nav>
 <!-- Navbar -->
 
-<div class="container-fluid" style="height: 100%" >
-	<form method="post">
+<div class="container-fluid">
   <div class="row">
     <div class="col-5">
+			<form>
   <div class="form-group">
-    <label for="exampleInputEmail1">Tempat Penulisan Surat</label>
-    <input type="text" class="form-control" id='tempatps' name="tempatps" aria-describedby="later" placeholder="">
-    <small id="emailHelp" class="form-text text-muted"></small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputname">Tanggal Penulisan Surat</label>
-    <input type="date" class="form-control" id="tanggalps" name="tanggalps" placeholder="">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
-    <label for="exampleInputname">Tujuan Surat</label>
-    <input type="text" class="form-control" id="tujuanps" name="tujuanps" placeholder="">
-  </div>
-   <div class="form-group">
-    <label for="exampleInputname">Alamat Tujuan</label>
-    <input type="text" class="form-control" id="alamatps" name="alamatps" placeholder="">
-  </div>
-   <div class="form-group">
-    <label for="exampleInputname">Salam Pembuka</label>
-    <input type="text" class="form-control" id="salampembuka" name="salampembuka" placeholder="">
-  </div>
-   <div class="form-group">
-    <label for="exampleInputname">Isi Surat</label>
-    <input type="text" class="form-control" id="isisurat" name="isisurat" placeholder="">
-  </div>
-   <div class="form-group">
-    <label for="exampleInputname">Salam Penutupan</label>
-    <input type="text" class="form-control" id="salampenutup" name="salampenutup" placeholder="">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputname">Pengirim Surat</label>
-    <input type="text" class="form-control" id="pengirimps" name="pengirimps" placeholder="">
-  </div>
-   <div class="form-group">
-    <label >Tanda Tangan</label>
-    <select class="form-control" id="tandatangan" name="tandatangan">
-    <option value="Ada">Ada</option>
-      <option value="Tidak Ada" selected>Tidak Ada</option>
-    </select>
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
 
-    </div>
-    <div>
-    	
     </div>
     <div class="col-7">
-      <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="font-size: 18px">
-		<div id='response_tempatps'>
-			<p>halo</p>
-		</div>
-		<div id='response_tanggalps'>
-			<p>halo</p>
-		</div>
-		<div id='response_tujuanps'>
-			<p>halo</p>
-		</div>
-		<div id='response_alamatps'>
-			<p>halo</p>
-		</div>
-		<div id='response_salampembuka'>
-			<p>halo</p>
-		</div>
-		<div id='response_isisurat'>
-			<p>halo</p>
-		</div>
-		<div id='response_salampenutup'>
-			<p>halo</p>
-		</div>
-		<div id='response_pengirimsurat'>
-			<p>halo</p>
-		</div>
-		<div id='response_tandatangan'>
-			<p>halo</p>
-		</div>
+      <div class="card shadow-lg p-3 mb-5 bg-white rounded">
+				dsadsad
 			</div>
-	<div class="form-row pull-right">
-    <div class="col-custom-1" style="margin-right: 52px;;margin-top: 10.5px;">
-      <select class="custom-select" id="tandatangan" name="tandatangan" style="width: 315px;height: 38px" >
-      	 <option selected>Folder</option>
-    	<option value="Perusahaan">Perusahaan</option>
-      <option value="Internal">Internal</option>
-       <option value="Personal">Personal</option>
-    </select>
-    </div>
-    <div class="col-custom-2">
-    	
-      <button type="submit" class="btn-lg btn-primary" >SAVE</button>
-     
-    </div>
-	</div>
+			<div class="offset-10">
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</div>
     </div>
   </div>
-  </form>
-</div>
 </div>
 
-        <script>
-        $(document).ready(function(){
-            $('#tempatps').keyup(function(){
-                var name = $('#tempatps').val();
 
-                $.ajax({
-                    type: 'post',
-                    data: {ajax: 1,name: tempatps},
-                    success: function(response){
-                        $('#response').text( response);
-                    }
-                });
-            });
-        });
-        </script>
+</form>
+
+
 
 
 
