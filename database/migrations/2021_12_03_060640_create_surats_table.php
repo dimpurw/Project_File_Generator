980c5c234063601e044cb43dcec649eba8fbb084
JupriTupi <luthfidians@gmail.com>
2021-12-03 13:32:26 +0700

    Model Surat

---
+++ database/migrations/2021_12_03_060640_create_surats_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surats', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('TempatPenulisan');
+            $table->date('TanggalPenulisan');
+            $table->string('TujuanSurat');
+            $table->string('AlamatTujuan');
+            $table->string('SalamPembuka');
+            $table->text('Isi');
+            $table->string('SalamPenutup');
+            $table->string('Pengirim');
+            $table->string('Ttd');
+            $table->string('JenisSurat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surats');
+    }
+}
