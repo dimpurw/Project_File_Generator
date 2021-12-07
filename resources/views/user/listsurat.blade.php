09c4a6fe68144b72f66937b8d701b309fb0018b4
FurqonWidaya <55239325+FurqonWidaya@users.noreply.github.com>
2021-12-07 15:50:41 +0700

    ..

--- resources/views/user/listsurat.blade.php
+++ resources/views/user/listsurat.blade.php
@@ -69,8 +69,10 @@ height: 44px;
           <div role="tabpanel" class="alert tab-pane active pb-4"  aria-expanded="true" >
                 <div class="card row" style="height: 117px;background-color: #FDC0C0">
           <div class="card-body"  style="border-left: 16px solid #FF3C3C!important;text-align: justify;padding-top: 35px;" >
+         <button type="button" class="close" data-dismiss="alert">&times;</button>
          <p class="card-text" style=" text-align: center; color:#FF0000; font-weight: 500;
-font-size: 24px;">Surat Perjanjian Kerjasama has been Deleted <i class="fa fa-trash-o"></i></p>
+font-size: 24px;">Surat has been Deleted <i class="fa fa-trash-o"></i></p>
+
           </div>
         </div>
           </div>
